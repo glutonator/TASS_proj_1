58dbb09a586566c4106b99e76f6d80529e1c0b62
glutonator <glutonator@gmail.com>
2017-11-23 21:00:22 +0100

    logarytm nie dziala

--- read_max_sklad_spoj.py
+++ read_max_sklad_spoj.py
@@ -2,10 +2,11 @@
 import networkx as nx, json
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy import stats
 
 print("poczatke wczytywania")
-H = nx.read_gml('C:\\Users\\Filip\Documents\\GitHub\\TASS_proj\\max_sklad_spoj.gml')
-#H = nx.read_gml('C:\\Users\\Filip\Documents\\GitHub\\TASS_proj\\test_new.gml')
+#H = nx.read_gml('C:\\Users\\Filip\Documents\\GitHub\\TASS_proj\\max_sklad_spoj.gml')
+H = nx.read_gml('C:\\Users\\Filip\Documents\\GitHub\\TASS_proj\\test_new.gml')
 print("koniec wczytywania")
 
 #assortatywnośc 1
@@ -54,11 +55,55 @@ for i in uuuu:
 
 fig = plt.subplots(1, 1)
 plt.plot(xxx,yyy,"ro")
-fig2 = plt.subplots(1, 1)
 
+fig2 = plt.subplots(1, 1)
 plt.plot(ggg.keys(),ggg.values(),"ro")
 plt.xscale('log')
 plt.yscale('log')
+
+
+fig3 = plt.subplots(1, 1)
+# m,b = np.polyfit(ggg.keys(), ggg.values(), 1)
+temp = list(ggg.keys())
+
+temp2=list(ggg.values())
+fit = np.polyfit(temp,temp2,1)
+fit_fn = np.poly1d(fit) 
+# fit_fn is now a function which takes in x and returns an estimate for y
+
+#tutaj rzeba zrobić tak by regresja liniowa była dopasowana już do z logarytmowanych dancyh, a nie do liniowych danych(osi) !!!!
+plt.plot(temp,temp2, 'yo', temp, fit_fn(temp), '--k')
+plt.xscale('log')
+plt.yscale('log')
+
+fig4 = plt.subplots(1, 1)
+temp3 = np.log10(temp)
+temp4 = np.log10(temp2)
+plt.plot(temp3,temp4,"ro")
+
+
+#tutaj cos się zwaliło z logarytmowaniem małych wartośći
+fig5 = plt.subplots(1, 1)
+fit = np.polyfit(temp3,temp4,1)
+fit_fn = np.poly1d(fit) 
+# # fit_fn is now a function which takes in x and returns an estimate for y
+
+# #tutaj rzeba zrobić tak by regresja liniowa była dopasowana już do z logarytmowanych dancyh, a nie do liniowych danych(osi) !!!!
+plt.plot(temp3,temp4, 'yo', temp3, fit_fn(temp3), '--k')
+
+#plt.show()
+
+
+# slope, intercept, r_value, p_value, std_err = stats.linregress(ggg.keys(), ggg.values())
+# print("r-squared:", r_value**2)
+
+# plt.plot(ggg.keys(), ggg.values(), 'o', label='original data')
+# plt.plot(ggg.keys(), intercept + slope*ggg.keys(), 'r', label='fitted line')
+# plt.legend()
+# plt.show()
+
+
+
 
 #plt.plot(uuuu,"ro")
 
--- test.py
+++ test.py
@@ -1,4 +1,36 @@
 
+import networkx as nx, json
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import stats
+
 
 print("SADsad")
 print("sadasdas")
+
+# np.random.seed(12345678)
+# x = np.random.random(10)
+# y = np.random.random(10)
+# slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+
+# print("r-squared:", r_value**2)
+
+# plt.plot(x, y, 'o', label='original data')
+# plt.plot(x, intercept + slope*x, 'r', label='fitted line')
+# plt.legend()
+# plt.show()
+
+
+x = [1,2,3,4]
+y = [3,5,7,10] # 10, not 9, so the fit isn't perfect
+
+print(np.log(x))
+fit = np.polyfit(x,y,1)
+fit_fn = np.poly1d(fit) 
+# fit_fn is now a function which takes in x and returns an estimate for y
+
+plt.plot(x,y, 'yo', x, fit_fn(x), '--k')
+#plt.xlim(0, 5)
+#plt.ylim(0, 12)
+
+plt.show()
